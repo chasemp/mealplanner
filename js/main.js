6fc748ea3042bc7b549d27af89235fadcfd88975
Chase Pettet <chase@owasp.org>
2025-09-05 13:46:34 -0500

    📱 Implement Mobile-First PWA Optimizations - Font size fix and mobile testing framework

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 class MealPlannerApp {
     constructor() {
         this.currentTab = 'recipes';
-        this.version = '2025.09.05.1326';
+        this.version = '2025.09.05.1346';
         this.itineraryViews = {};
         this.calendarViews = {};
         this.recipeManager = null;
