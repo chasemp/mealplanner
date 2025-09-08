d6ccfa2c266866bdb101df294a3deaf9884b154b
Chase Pettet <chase@owasp.org>
2025-09-08 15:57:36 -0500

    FIXED: Settings dropdown positioning - reduced width and right margin to prevent cutoff on desktop

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 class MealPlannerApp {
     constructor() {
         this.currentTab = 'dinner';
-        this.version = '2025.09.08.1551';
+        this.version = '2025.09.08.1556';
         this.itineraryViews = {};
         this.calendarViews = {};
         this.recipeManager = null;
