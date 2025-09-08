fcfc8596accd85feed31dc0c0947ea764e6eaa7a
Chase Pettet <chase@owasp.org>
2025-09-08 15:42:18 -0500

    AGGRESSIVE cache busting: Add .nojekyll, build timestamp, version in title, force rebuild

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 class MealPlannerApp {
     constructor() {
         this.currentTab = 'dinner';
-        this.version = '2025.09.08.1455';
+        this.version = '2025.09.08.1542';
         this.itineraryViews = {};
         this.calendarViews = {};
         this.recipeManager = null;
