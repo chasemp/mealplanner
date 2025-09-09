e5115e9e6ff30beb477bf8a904e100ccdd4344d2
Chase Pettet <chase@owasp.org>
2025-09-09 14:45:20 -0500

    🔧 FIX: Update cache-busting version to resolve ingredients loading issue
    - Updated all files to consistent version 2025.09.09.1441
    - Fixes version mismatch that prevented ingredients from loading in demo mode
    - Ensures all script/link tags have matching cache-busting parameters

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 class MealPlannerApp {
     constructor() {
         this.currentTab = 'dinner';
-        this.version = '2025.09.09.1107';
+        this.version = '2025.09.09.1441';
         this.itineraryViews = {};
         this.calendarViews = {};
         this.recipeManager = null;
