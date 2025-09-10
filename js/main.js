45732f4e0efd9b1f49e3569a0d58d0f0ecd683de
chasemp <chasemp@users.noreply.github.com>
2025-09-10 02:47:56 +0000

    deploy: f9ec3004120617908063a796fc52f4f385aedf29

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 class MealPlannerApp {
     constructor() {
         this.currentTab = 'dinner';
-        this.version = '2025.09.09.2142';
+        this.version = '2025.09.09.2145';
         this.itineraryViews = {};
         this.calendarViews = {};
         this.recipeManager = null;
